a01ff3572e375d94ca0f22f313d744bb941b9ab7
Walid Adebayo <adebayowalid@gmail.com>
2025-02-16 02:22:57 +0000

    refactor: update Jest configuration to use ES module syntax and enhance module file extensions

--- package/jest.config.js
+++ package/jest.config.js
@@ -1,11 +1,13 @@
-module.exports = {
+import { defaults } from 'jest-config';
+
+export default {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
